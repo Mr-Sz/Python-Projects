6f2efb5da3c14c1c0cfe38561b1b34019ede8bf8
Mr-Sz <shahroz14k@gmail.com>
2023-04-08 15:00:40 +0530

    changed repo file structure and added some Readmes

---
+++ job_webscrapper/README.md
@@ -0,0 +1,29 @@
+# Job Listings Web Scraper
+
+This Python script scrapes job listings from the TimesJobs website for the keyword "python". It checks if the job was published "few" days ago, and if so, it prints out the company name and required skills for the job.
+
+## Prerequisites
+
+To run this script, you need to have Python 3.x installed on your machine. You also need to install the following Python packages:
+
+- `beautifulsoup4`
+- `requests`
+- `lxml`
+
+You can install these packages using `pip`:
+
+
+## Usage
+
+To use this script, simply run the `main.py` file:
+
+
+The script will output the company names and required skills for job listings that were published "few" days ago.
+
+## Contributing
+
+If you'd like to contribute to this project, feel free to fork the repository and submit a pull request. contributions are welcome!
+
+## License
+
+This project is licensed under the MIT License. See the `LICENSE` file for details.
--- job_webscrapper/main.py
+++ job_webscrapper/main.py
@@ -1,15 +1,33 @@
 from bs4 import BeautifulSoup
 import requests
 
+# Fetch HTML content from website
 html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
+
+# Parse HTML content using BeautifulSoup library
 soup = BeautifulSoup(html_text, 'lxml')
+
+# Find all job listings on the page
 jobs = soup.find_all('li', class_ = 'clearfix job-bx wht-shd-bx')
+
+# Loop through each job listing and print out details for jobs that were published "few" days ago
 for job in jobs: 
+    # Get the published date of the job listing
     published_date = job.find('span',class_ ='sim-posted').text.split('\n')[-2]
+    
+    # Check if the job was published "few" days ago
     if 'few' in published_date: 
+        # Get the company name from the job listing
         company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ','').replace('\r','').replace('\n','').split('(')[0]
+        
+        # Get the required skills for the job listing
         skills = job.find('span', class_ = 'srp-skills').text.replace(' ','').replace('"','').replace('\r','').replace('\n','')
+        
+        # Print out the details for the job listing
         print(f'''
         Company Name: {company_name}
         Required Skills: {skills}
         ''')
+
+# Add a commit message to indicate what changes were made to the code
+print("Added commit messages to code")
