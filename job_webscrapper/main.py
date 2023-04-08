8fa76587794792a04ec438acddbaba86f500dff9
Mr-Sz <shahroz14k@gmail.com>
2023-04-08 20:02:05 +0530

    added features to webscraper

--- job_webscrapper/main.py
+++ job_webscrapper/main.py
@@ -1,6 +1,11 @@
 from bs4 import BeautifulSoup
 import requests
 
+
+print('Enter some Skill that you are not familiar with:')
+unfamiliar_skill = input('>').lower().replace(' ','')
+print(f'filtering out {unfamiliar_skill}...')
+
 # Fetch HTML content from website
 html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
 
@@ -18,16 +23,16 @@ for job in jobs:
     # Check if the job was published "few" days ago
     if 'few' in published_date: 
         # Get the company name from the job listing
-        company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ','').replace('\r','').replace('\n','').split('(')[0]
+        company_name = job.find('h3', class_='joblist-comp-name').text.replace('\r','').replace('\n','').split('(')[0]
         
         # Get the required skills for the job listing
-        skills = job.find('span', class_ = 'srp-skills').text.replace(' ','').replace('"','').replace('\r','').replace('\n','')
+        skills = job.find('span', class_ = 'srp-skills').text.replace(' ','').replace('"','').replace('\r','').replace('\n','').lower()
+        more_info = job.header.h2.a['href'] # I do this extract value of 'href' in 'a'tag
         
-        # Print out the details for the job listing
-        print(f'''
-        Company Name: {company_name}
-        Required Skills: {skills}
-        ''')
-
-# Add a commit message to indicate what changes were made to the code
-print("Added commit messages to code")
+        # check is unfamiliar skill is not present
+        if unfamiliar_skill not in skills: 
+        
+            print(f'Company Name: {company_name.strip()}')
+            print(f'Required Skills: {skills}')
+            print(f'more_info: {more_info}')
+            print('')
